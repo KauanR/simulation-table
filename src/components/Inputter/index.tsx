73f4693edd8693194ea7e95b97fba330576ecbb2
Kai <kau.matosr@gmail.com>
2023-03-16 23:23:56 -0300

    Inputter component style improvement

--- src/components/Inputter/index.tsx
+++ src/components/Inputter/index.tsx
@@ -19,7 +19,7 @@ export const Inputter = ({ onSubmit }: Props) => {
         formState: { errors: formErrors, isValid: formIsValid }
     } = useForm<Inputs>({
         resolver: yupResolver(Yup.object().shape({
-            simulationTime: Yup.number().required('O Tempo de Simulação é obrigatório'),
+            simulationTime: Yup.number().typeError('O Tempo de Simulação é obrigatório').required('O Tempo de Simulação é obrigatório'),
             tecList: Yup.array().of(Yup.string()).min(1, 'É necessário ao menos 1 item'),
             tsList: Yup.array().of(Yup.string()).min(1, 'É necessário ao menos 1 item')
         })),
@@ -42,79 +42,81 @@ export const Inputter = ({ onSubmit }: Props) => {
     };
 
     return (
-        <Card className='inputter'>
-            <CardContent>
-                <div className='title'>
-                    <Typography variant='h3'>
-                        Teoria das filas
-                    </Typography>
-                    <Typography variant='subtitle1' color='text.secondary'>
-                        Para começar, por favor insira os dados necessários logo abaixo.
-                    </Typography>
-                </div>
+        <div id='inputter'>
+            <Card>
+                <CardContent>
+                    <div className='title'>
+                        <Typography variant='h3'>
+                            Teoria das filas
+                        </Typography>
+                        <Typography variant='subtitle1' color='text.secondary'>
+                            Para começar, por favor insira os dados necessários logo abaixo.
+                        </Typography>
+                    </div>
 
-                <form id='inputter-form' onSubmit={handleSubmit(submitHandler)}>
-                    <Controller
-                        name='simulationTime'
-                        control={control}
-                        render={({ field }) => (
-                            <TextField
-                                {...field}
-                                type='number'
-                                label='Tempo de Simulação'
-                                variant='outlined'
-                                autoFocus
-                                error={!!formErrors.simulationTime}
-                                helperText={formErrors.simulationTime?.message}
-                            />
-                        )}
-                    />
+                    <form id='inputter-form' onSubmit={handleSubmit(submitHandler)}>
+                        <Controller
+                            name='simulationTime'
+                            control={control}
+                            render={({ field }) => (
+                                <TextField
+                                    {...field}
+                                    type='number'
+                                    label='Tempo de Simulação'
+                                    variant='outlined'
+                                    autoFocus
+                                    error={!!formErrors.simulationTime}
+                                    helperText={formErrors.simulationTime?.message}
+                                />
+                            )}
+                        />
 
-                    <Controller
-                        name='tecList'
-                        control={control}
-                        render={({ field }) => (
-                            <MuiChipsInput
-                                {...field}
-                                inputProps={{ type: 'number' }}
-                                disableEdition
-                                variant='outlined'
-                                label='Tempo Entre Chegadas(TEC)'
-                                placeholder='Digite e aperte ENTER para inserir'
-                                error={!!formErrors.tecList}
-                                helperText={formErrors.tecList?.message}
-                            />
-                        )}
-                    />
+                        <Controller
+                            name='tecList'
+                            control={control}
+                            render={({ field }) => (
+                                <MuiChipsInput
+                                    {...field}
+                                    inputProps={{ type: 'number' }}
+                                    disableEdition
+                                    variant='outlined'
+                                    label='Tempo Entre Chegadas(TEC)'
+                                    placeholder='Digite e aperte ENTER para inserir'
+                                    error={!!formErrors.tecList}
+                                    helperText={formErrors.tecList?.message}
+                                />
+                            )}
+                        />
 
-                    <Controller
-                        name='tsList'
-                        control={control}
-                        render={({ field }) => (
-                            <MuiChipsInput
-                                {...field}
-                                inputProps={{ type: 'number' }}
-                                disableEdition
-                                variant='outlined'
-                                label='Tempo de Serviço(TS)'
-                                placeholder='Digite e aperte ENTER para inserir'
-                                error={!!formErrors.tsList}
-                                helperText={formErrors.tsList?.message}
-                            />
-                        )}
-                    />
-                </form>
+                        <Controller
+                            name='tsList'
+                            control={control}
+                            render={({ field }) => (
+                                <MuiChipsInput
+                                    {...field}
+                                    inputProps={{ type: 'number' }}
+                                    disableEdition
+                                    variant='outlined'
+                                    label='Tempo de Serviço(TS)'
+                                    placeholder='Digite e aperte ENTER para inserir'
+                                    error={!!formErrors.tsList}
+                                    helperText={formErrors.tsList?.message}
+                                />
+                            )}
+                        />
+                    </form>
 
-                <Button
-                    type='submit'
-                    variant='contained'
-                    size='large'
-                    form='inputter-form'
-                    disabled={!formIsValid}
-                >
-                    Simular
-                </Button>
-            </CardContent>
-        </Card>
+                    <Button
+                        type='submit'
+                        variant='contained'
+                        size='large'
+                        form='inputter-form'
+                        disabled={!formIsValid}
+                    >
+                        Simular
+                    </Button>
+                </CardContent>
+            </Card>
+        </div>
     );
 };
